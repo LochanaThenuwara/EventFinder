c33618a559934933d6facb3c8b79f8d8491ab584
LochanaThenuwara <kavindilochana@gmail.com>
2016-12-21 10:01:59 +0530

    addNameSearch

--- ui.py
+++ ui.py
@@ -3,35 +3,46 @@ import xml.etree.ElementTree as ET
 import tkMessageBox
 
 
-wx_file= open('C:/ProjectSE/parser/AISTargetListDialog.cpp','r')
-map={'onclick':["wxEVT_COMMAND_BUTTON_CLICKED", "wxEVT_BUTTON","EVT_BUTTON"]}
+# wx_file= open('C:/ProjectSE/WxLoginForm/WxLoginForm/FormLogin.cpp','r')
+# wx_file = open('C:/ProjectSE/wxRegitrationForm/WxRegitrationForm/WxRegitrationForm/FormReg.cpp', 'r')
+wx_file= open('C:/ProjectSE/OpenCPN/src/AISTargetListDialog.cpp','r')
+# wx_file= open('C:/ProjectSE/parser/AISTargetListDialog.cpp','r')
+# wx_file= open('C:/ProjectSE/parser/chart1.cpp','r')
+map={'onclick':["wxEVT_COMMAND_BUTTON_CLICKED", "wxEVT_BUTTON","EVT_BUTTON","EVT_CHECKBOX","wxEVT_COMMAND_CHECKBOX_ CLICKED","EVT_RADIOBUTTON","wxEVT_COMMAND_ RADIOBUTTON_ SELECTED"],
+     'onchange':["EVT_TEXT_ENTER","wxEVT_COMMAND_TEXT_ENTER"],
+     'onkeyup':["wxEVT_COMMAND_TEXT_UPDATED","EVT_TEXT"]}
 
 
 def xmlParser(ui_name):
-    tree = ET.parse("C:\wamp\www\BootstrapConverter\event.xml")
+    tree = ET.parse("C:\wamp\www\BootstrapConverter\event1.xml")
     root=tree.getroot()
+    arrayItems=root.findall('item')
 
-    for ui in root.findall(ui_name):
-        events = ui.findall('item')
-        for i in events:
-            e=i.find('event')
-            method=i.find('method')
-            print e.text
-            event=e.text
-            print method.text
+    for item in arrayItems:
+        obj=item.findall(ui_name)
+        for ui in obj:
+            events = ui.findall('item')
+            for i in events:
+                e=i.find('event')
+                method=i.find('method')
+                print e.text,"999999999999"
+                event=e.text
+                print method.text
 
-            mapFunction(event)
+                mapFunction(event)
 
-            lineNo=findMethod(method.text)
-            tkMessageBox.showinfo("Show Details",
-                                  "Method Name is :   " + method.text + "\nEvent is:   " + event+ "\nMapped Bootstrap Event is:   "
-                                  + mapFunction(event)+"\nLine no is:   " + str(lineNo))
+                lineNo=findMethod(method.text)
+                tkMessageBox.showinfo("Show Details",
+                                      "Method Name is :   " + method.text + "\nEvent is:   " + event+ "\nMapped Bootstrap Event is:   "
+                                      + mapFunction(event)+"\nLine no is:   " + str(lineNo))
 
-#
+    #
 
 def findMethod(method):
-    count=0;
+    count=1;
+    print method,"xxxx"
     for line in wx_file:
+
         if "void "+method in line:
             print line
             print count
@@ -41,7 +52,13 @@ def findMethod(method):
 
 
 def mapFunction(event):
-    return event
+    for key, value in map.iteritems():
+        for i in range(0,len(value)):
+            if event==value[i]:
+
+                return key
+            else:
+                return ""
 
 
 def show_entry_fields():
@@ -50,7 +67,10 @@ def show_entry_fields():
     print("UI name: %s" % ui_name)
     xmlParser(ui_name)
 
-
+def show_objects():
+    file=open("C:/ProjectSE/Parser/foundUi.txt",'r')
+    for line in file:
+        print line
 
 
 
@@ -65,9 +85,9 @@ e1.grid(row=0, column=1)
 # e2.grid(row=1, column=1)
 
 
-Button(master, text='Show Method', command=show_entry_fields).grid(row=3, column=0, sticky=W, pady=4)
-Button(master, text='Quit', command=master.quit).grid(row=3, column=1, sticky=W, pady=4)
-
+Button(master, text='Show Object List', command=show_objects).grid(row=3, column=0, sticky=W, pady=4)
+Button(master, text='Show Method', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
+Button (master, text='Quit', command=master.quit).grid(row=3, column=2, sticky=W, pady=4)
 
 
 mainloop( )
